e63638187607cd6c83f4f42d84c1e1ed594f716a
kiarash-sh81 <kiarashshahroudi@gmail.com>
2023-02-10 12:10:04 +0330

    all methods Done

--- Todo.ts
+++ Todo.ts
@@ -54,7 +54,11 @@ class TodoController extends TodoRepository{
     }
 
     public createTodo(todo: createTodoDTO): ResponseMethod {
-        const id = this.Todos.length+1
+        let id = this.Todos.length+1;
+        if(this.Todos.length > 0){ //?   never generate a duplicate id
+            let lastOne : number = this.Todos.length-1
+            id = this.Todos[lastOne].id+1
+        }
         let newTodo: Todo ={
             id,
             title: todo.title,
@@ -63,7 +67,34 @@ class TodoController extends TodoRepository{
         this.Todos.push(newTodo)
         return {message: "created"}
     }
+    public getAll(): Todo[] {
+        return this.Todos;
+    }
+
+    public getById(id: number): Todo | ResponseMethod {
+        const todo = this.Todos.find(todo => todo.id == id);
+        if(!todo) return {message: "todo not founded"}
+        return todo
+    }
+
+    public deleteTodo(id: number): ResponseMethod {
+        const todo = this.getById(id);
+        const NewTodos = this.Todos.filter(todo => todo.id !== id);
+        this.Todos = NewTodos;
+        return {message: "todo deleted"}
+    }
 }
 
 const todo = new TodoController();
 console.log(todo.createTodo({title: "work" , state: State.START}));
+console.log(todo.createTodo({title: "course" , state: State.START}));
+console.log(todo.createTodo({title: "gym" , state: State.TOP_LIST}));
+console.log(todo.getAll());
+console.log(todo.getById(3));
+console.log(todo.deleteTodo(2));
+console.log(todo.getAll());
+console.log(todo.createTodo({title: "class" , state: State.TOP_LIST}));
+console.log(todo.getAll());
+
+
+
